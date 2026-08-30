f33f1612e586380418e62f42fd63a24f6d5199e9
21CNCStudio <21cncstudio@gmail.com>
2026-08-30 02:33:32 +0100

    Return the newest support errors

--- test/test_web_events_utils/test_web_events_utils.cpp
+++ test/test_web_events_utils/test_web_events_utils.cpp
@@ -1,6 +1,7 @@
 #include <unity.h>
 
 #include <ArduinoJson.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "core/Logger.h"
@@ -31,23 +32,81 @@ Logger::RecentEntry make_entry(uint32_t ms,
 
 void test_web_events_utils_fill_recent_errors_json_filters_and_limits() {
     const Logger::RecentEntry entries[] = {
-        make_entry(10, Logger::Info, "Main", "skip"),
-        make_entry(20, Logger::Warn, "WiFi", "warn"),
+        make_entry(10, Logger::Warn, "Main", "older warning"),
+        make_entry(20, Logger::Info, "Main", "skip"),
         make_entry(30, Logger::Error, "MQTT", "boom"),
+        make_entry(35, Logger::Debug, "UI", "skip debug"),
         make_entry(40, Logger::Warn, "", ""),
+        make_entry(50, Logger::Info, "WiFi", "skip trailing info"),
     };
 
     ArduinoJson::JsonDocument doc;
     const size_t added = WebEventsUtils::fillRecentErrorsJson(
-        doc["errors"].to<ArduinoJson::JsonArray>(), entries, 4, 2);
+        doc["errors"].to<ArduinoJson::JsonArray>(), entries, 6, 2);
 
     TEST_ASSERT_EQUAL_UINT32(2, added);
     TEST_ASSERT_EQUAL_UINT32(2, doc["errors"].size());
-    TEST_ASSERT_EQUAL_STRING("W", doc["errors"][0]["level"].as<const char *>());
-    TEST_ASSERT_EQUAL_STRING("WiFi", doc["errors"][0]["tag"].as<const char *>());
-    TEST_ASSERT_EQUAL_STRING("warn", doc["errors"][0]["message"].as<const char *>());
-    TEST_ASSERT_EQUAL_STRING("E", doc["errors"][1]["level"].as<const char *>());
-    TEST_ASSERT_EQUAL_STRING("MQTT", doc["errors"][1]["tag"].as<const char *>());
+    TEST_ASSERT_EQUAL_UINT32(30, doc["errors"][0]["ts_ms"].as<uint32_t>());
+    TEST_ASSERT_EQUAL_STRING("E", doc["errors"][0]["level"].as<const char *>());
+    TEST_ASSERT_EQUAL_STRING("MQTT", doc["errors"][0]["tag"].as<const char *>());
+    TEST_ASSERT_EQUAL_STRING("boom", doc["errors"][0]["message"].as<const char *>());
+    TEST_ASSERT_EQUAL_UINT32(40, doc["errors"][1]["ts_ms"].as<uint32_t>());
+    TEST_ASSERT_EQUAL_STRING("W", doc["errors"][1]["level"].as<const char *>());
+    TEST_ASSERT_EQUAL_STRING("SYSTEM", doc["errors"][1]["tag"].as<const char *>());
+    TEST_ASSERT_EQUAL_STRING("", doc["errors"][1]["message"].as<const char *>());
+}
+
+void test_web_events_utils_recent_errors_keeps_all_when_limit_exceeds_count() {
+    const Logger::RecentEntry entries[] = {
+        make_entry(10, Logger::Info, "Main", "skip"),
+        make_entry(20, Logger::Warn, "WiFi", "warning"),
+        make_entry(30, Logger::Error, "GT911", "failure"),
+    };
+
+    ArduinoJson::JsonDocument doc;
+    const size_t added = WebEventsUtils::fillRecentErrorsJson(
+        doc["errors"].to<ArduinoJson::JsonArray>(), entries, 3, 8);
+
+    TEST_ASSERT_EQUAL_UINT32(2, added);
+    TEST_ASSERT_EQUAL_UINT32(20, doc["errors"][0]["ts_ms"].as<uint32_t>());
+    TEST_ASSERT_EQUAL_UINT32(30, doc["errors"][1]["ts_ms"].as<uint32_t>());
+}
+
+void test_web_events_utils_recent_errors_handles_empty_inputs() {
+    const Logger::RecentEntry entry =
+        make_entry(10, Logger::Warn, "Main", "warning");
+    const Logger::RecentEntry info =
+        make_entry(20, Logger::Info, "Main", "skip");
+    ArduinoJson::JsonDocument doc;
+    const ArduinoJson::JsonArray errors =
+        doc["errors"].to<ArduinoJson::JsonArray>();
+
+    TEST_ASSERT_EQUAL_UINT32(
+        0, WebEventsUtils::fillRecentErrorsJson(errors, nullptr, 1, 1));
+    TEST_ASSERT_EQUAL_UINT32(
+        0, WebEventsUtils::fillRecentErrorsJson(errors, &entry, 0, 1));
+    TEST_ASSERT_EQUAL_UINT32(
+        0, WebEventsUtils::fillRecentErrorsJson(errors, &entry, 1, 0));
+    TEST_ASSERT_EQUAL_UINT32(
+        0, WebEventsUtils::fillRecentErrorsJson(errors, &info, 1, 1));
+    TEST_ASSERT_EQUAL_UINT32(0, errors.size());
+}
+
+void test_web_events_utils_recent_errors_preserves_order_across_uptime_rollover() {
+    const Logger::RecentEntry entries[] = {
+        make_entry(UINT32_MAX - 8U, Logger::Warn, "I2C", "older warning"),
+        make_entry(UINT32_MAX - 1U, Logger::Error, "GT911", "before rollover"),
+        make_entry(2, Logger::Warn, "I2C", "after rollover"),
+    };
+
+    ArduinoJson::JsonDocument doc;
+    const size_t added = WebEventsUtils::fillRecentErrorsJson(
+        doc["errors"].to<ArduinoJson::JsonArray>(), entries, 3, 2);
+
+    TEST_ASSERT_EQUAL_UINT32(2, added);
+    TEST_ASSERT_EQUAL_UINT32(
+        UINT32_MAX - 1U, doc["errors"][0]["ts_ms"].as<uint32_t>());
+    TEST_ASSERT_EQUAL_UINT32(2, doc["errors"][1]["ts_ms"].as<uint32_t>());
 }
 
 void test_web_events_utils_fill_events_json_uses_web_event_rules_and_fallbacks() {
@@ -73,6 +132,9 @@ void test_web_events_utils_fill_events_json_uses_web_event_rules_and_fallbacks()
 int main(int, char **) {
     UNITY_BEGIN();
     RUN_TEST(test_web_events_utils_fill_recent_errors_json_filters_and_limits);
+    RUN_TEST(test_web_events_utils_recent_errors_keeps_all_when_limit_exceeds_count);
+    RUN_TEST(test_web_events_utils_recent_errors_handles_empty_inputs);
+    RUN_TEST(test_web_events_utils_recent_errors_preserves_order_across_uptime_rollover);
     RUN_TEST(test_web_events_utils_fill_events_json_uses_web_event_rules_and_fallbacks);
     return UNITY_END();
 }
